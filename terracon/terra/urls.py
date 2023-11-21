c0ee3bbcd9dfd24ec373c97ef3e467dd48c58d76
banana5512 <banana5512@naver.com>
2023-11-21 20:28:06 +0900

    django init 231121

--- terracon/terra/urls.py
+++ terracon/terra/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.main, name='main'),  # 뷰 함수와 URL 패턴을 연결
+    path('region/',views.region,name='region'),
+    path('instance/',views.instance,name='instance'),
 ]
--- terracon/terra/views.py
+++ terracon/terra/views.py
@@ -4,4 +4,6 @@ def main(request):
     return render(request,'0_main.html')
 def region(request):
     return render(request,'1_1_region.html')
+def instance(request):
+    return render(request,'1_2_ins_type.html')
 
