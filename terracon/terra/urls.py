816773f1d8be152e5bf91722354ac886f187b2b3
banana5512 <banana5512@naver.com>
2023-11-23 15:54:00 +0900

    대규모 패치

--- terracon/terra/urls.py
+++ terracon/terra/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('region/',views.region,name='region'),
     path('instance/',views.instance,name='instance'),
     path('key/',views.key, name='key'),
+    path('home/', views.home, name='home'),  # 뷰 함수와 URL 패턴을 연결
+    path('list/',views.list,name='list'),
+    path('step1/',views.step1,name='step1'),
+    path('step2/',views.step2, name='step2'),
 ]
--- terracon/terra/views.py
+++ terracon/terra/views.py
@@ -3,9 +3,20 @@ from django.shortcuts import render,redirect
 def main(request):
     return render(request,'0_main.html')
 def region(request):
-    return render(request,'1_1_region.html')
+    tfstr = "스트링 제대로 처리 되나요"
+    return render(request,'1_1_region.html',{'tfstring' : tfstr})
 def instance(request):
     return render(request,'1_2_ins_type.html')
 def key(request):
     return render(request,'1_3_key.html')
 
+
+def home(request):
+    return render(request,'0_home.html')
+def list(request):
+    tfstr = "스트링 제대로 처리 되나요"
+    return render(request,'1_list.html',{'tfstring' : tfstr})
+def step1(request):
+    return render(request,'2_step1.html')
+def step2(request):
+    return render(request,'3_step2.html')
