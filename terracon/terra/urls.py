0e25643cac32c823d648729e7435572010a986d7
njs <smile0105@kakao.com>
2023-11-23 16:06:31 +0900

    대규모 패치 이후 버그 수정

--- terracon/terra/urls.py
+++ terracon/terra/urls.py
@@ -2,10 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main, name='main'),  # 뷰 함수와 URL 패턴을 연결
-    path('region/',views.region,name='region'),
-    path('instance/',views.instance,name='instance'),
-    path('key/',views.key, name='key'),
     path('home/', views.home, name='home'),  # 뷰 함수와 URL 패턴을 연결
     path('list/',views.list,name='list'),
     path('step1/',views.step1,name='step1'),
