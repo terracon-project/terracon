d8de6ee36d61e2db76a1b429130941f078c6888e
banana5512 <banana5512@naver.com>
2023-11-28 14:15:29 +0900

    mainurl안막음_b_views.py

--- terracon/terra/views.py
+++ terracon/terra/views.py
@@ -13,54 +13,54 @@ def home(request):
     return render(request,'0_home.html')
 
 def main(request):
-    if request.method == 'POST' or request.GET.get('from_button'):
-        # 사용자로부터 입력 받은 AWS 액세스 키, 시크릿 키, 리전 정보
-        if request.method == 'POST':
-            aws_access_key = request.POST.get('aws_access_key')
-            aws_secret_key = request.POST.get('aws_secret_key')
-            aws_region = request.POST.get('aws_region')
-            request.session['aws_region'] = aws_region
-            request.session['aws_access_key'] = aws_access_key
-            request.session['aws_secret_key'] = aws_secret_key
+    
+    # 사용자로부터 입력 받은 AWS 액세스 키, 시크릿 키, 리전 정보
+    if request.method == 'POST':
+        aws_access_key = request.POST.get('aws_access_key')
+        aws_secret_key = request.POST.get('aws_secret_key')
+        aws_region = request.POST.get('aws_region')
+        request.session['aws_region'] = aws_region
+        request.session['aws_access_key'] = aws_access_key
+        request.session['aws_secret_key'] = aws_secret_key
+    
+    # AWS SDK를 사용하여 EC2 클라이언트 생성
+    try:
+        ec2_client = boto3.client(
+        'ec2',
+            aws_access_key_id=request.session.get('aws_access_key'),
+            aws_secret_access_key=request.session.get('aws_secret_key'),
+            region_name=request.session.get('aws_region')
+        )
         
-        # AWS SDK를 사용하여 EC2 클라이언트 생성
-        try:
-            ec2_client = boto3.client(
-            'ec2',
-                aws_access_key_id=request.session.get('aws_access_key'),
-                aws_secret_access_key=request.session.get('aws_secret_key'),
-                region_name=request.session.get('aws_region')
-            )
-            
 
-            # 인스턴스 목록 가져오기
-            instances = ec2_client.describe_instances()
+        # 인스턴스 목록 가져오기
+        instances = ec2_client.describe_instances()
 
-            # 필요한 정보 추출 (인스턴스 이름, ID, 상태 등)
-            instance_info_list = []
-            for reservation in instances['Reservations']:
-                for instance in reservation['Instances']:
-                    instance_id = instance['InstanceId']
-                    instance_state = instance['State']['Name']
+        # 필요한 정보 추출 (인스턴스 이름, ID, 상태 등)
+        instance_info_list = []
+        for reservation in instances['Reservations']:
+            for instance in reservation['Instances']:
+                instance_id = instance['InstanceId']
+                instance_state = instance['State']['Name']
 
-                    # 이름 정보 가져오기
-                    for tag in instance['Tags']:
-                        if tag['Key'] == 'Name':
-                            instance_name = tag['Value']
-                            break
-                    else:
-                        instance_name = 'N/A'  # 이름이 없는 경우를 대비하여 기본값 설정
+                # 이름 정보 가져오기
+                for tag in instance['Tags']:
+                    if tag['Key'] == 'Name':
+                        instance_name = tag['Value']
+                        break
+                else:
+                    instance_name = 'N/A'  # 이름이 없는 경우를 대비하여 기본값 설정
 
-                    instance_info_list.append({'id': instance_id, 'name': instance_name, 'state': instance_state})
-        except:
-            print('액세스 키 또는 시크릿 키가 잘못되었습니다.')
-            return redirect('home')
+                instance_info_list.append({'id': instance_id, 'name': instance_name, 'state': instance_state})
+    except:
+        print('액세스 키 또는 시크릿 키가 잘못되었습니다.')
+        return redirect('home')
 
-        # HTML 템플릿에 데이터 전달
-        context = {'instances_info': instance_info_list}   
-        return render(request, '1_main.html', context)
-    else:
-         redirect('home')
+    # HTML 템플릿에 데이터 전달
+    context = {'instances_info': instance_info_list}   
+    return render(request, '1_main.html', context)
+
+         
 def instances_view(request):
     if request.method == 'POST':
         data = json.loads(request.body)
